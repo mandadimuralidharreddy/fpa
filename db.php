105818a74f143b2bcf8b887f7e2d81ccbaac7d55
mandadimuralidharreddy <mandadimuralidharreddy@gmail.com>
2015-07-10 01:09:12 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "fpaData.db.5802143.hostedresource.com";
-$username = "fpaData";
-$password = "FPAadmin@123";
-$dbname = "fpaData";
+$servername = "mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
+$username = "adminA9ARczK";
+$password = "4PeDU7LkX2Uj";
+$dbname = "fpa";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
